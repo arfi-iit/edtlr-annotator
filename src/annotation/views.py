df98d5263c0d393af86365579435b6071f65e404
RePierre <petru.rebeja@gmail.com>
2024-01-21 20:26:15 +0200

    Fixed layout of the controls on the validation page

--- src/annotation/views.py
+++ src/annotation/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def validate(request):
-    return render(request, "annotation/validate.html")
+    return render(request, "annotation/validate.html",
+                  {'image_path': 'annotation/data/128.png'})
