53240f87f29798b7245298899269ce3acbd42ef4
RePierre <petru.rebeja@gmail.com>
2024-01-24 20:08:42 +0200

    Added OcrResult model

--- src/annotation/models.py
+++ src/annotation/models.py
@@ -1,3 +1,11 @@
+"""Contains the models."""
 from django.db import models
 
+
 # Create your models here.
+class OcrResult(models.Model):
+    """Represents a pair of (page image, OCR text)."""
+
+    page_no = models.IntegerField(primary_key=True)
+    image_path = models.CharField(unique=True, null=False, max_length=1024)
+    text = models.CharField(max_length=4096, null=False)
