5a6dba78a40eb5c0f672c485485e3b8c36d6db75
RePierre <petru.rebeja@gmail.com>
2024-01-24 20:11:20 +0200

    Added Json view for OcrResult

--- src/annotation/urls.py
+++ src/annotation/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "annotation"
-urlpatterns = [path("", views.validate, name="index")]
+urlpatterns = [
+    path("", views.validate, name="index"),
+    path("api/ocrresults", views.get_ocr_result, name="get_ocr_result")
+]
--- src/annotation/views.py
+++ src/annotation/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf.urls.static import static
+from .models import OcrResult
 
 
 # Create your views here.
 def validate(request):
-    return render(request, "annotation/validate.html",
-                  {'image_path': 'annotation/data/128.png'})
+    return render(request, "annotation/validate.html")
+
+
+def get_ocr_result(request):
+    model = OcrResult.objects.get(pk=128)
+    data = {
+        'text': model.text,
+        'image_path': f'/static/annotation/{model.image_path}'
+    }
+    return JsonResponse(data)
