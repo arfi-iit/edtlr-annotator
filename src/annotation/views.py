e2ef40788035bc0614557f11753f01267eb500c2
RePierre <petru.rebeja@gmail.com>
2024-01-27 19:59:43 +0200

    Marked all views as login required

--- src/annotation/views.py
+++ src/annotation/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 from .models import OcrResult
 
-
 # Create your views here.
+
+
+@login_required
 def validate(request):
     return render(request, "annotation/validate.html")
 
 
+@login_required
 def get_ocr_result(request, page_no: int):
     model = OcrResult.objects.get(pk=page_no)
     data = {
@@ -18,9 +22,11 @@ def get_ocr_result(request, page_no: int):
     return JsonResponse(data)
 
 
+@login_required
 def save_annotation(request):
     return redirect("annotation:index")
 
 
+@login_required
 def mark_valid(request):
     return redirect("annotation:index")
