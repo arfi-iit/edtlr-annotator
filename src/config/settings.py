afb823fcb6b61c0cdca9b572b63c705fa617f49d
RePierre <petru.rebeja@gmail.com>
2024-03-15 20:10:48 +0200

    Read settings from environment file

--- src/config/settings.py
+++ src/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = env('STATIC_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -127,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/annotate'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+
+# Application constants
+MAX_CONCURRENT_ANNOTATORS = env('MAX_CONCURRENT_ANNOTATORS')
