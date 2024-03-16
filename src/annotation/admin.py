4445a016717e1565b0839b48cf42f09824490b46
RePierre <petru.rebeja@gmail.com>
2024-03-16 18:12:46 +0200

    Altered database model
    - Added Volume model to be able to group pages with the same page
    number
    - Made property `contents` of the `Annotation` model a nullable text
    field
    - Added unique constraint on `volume_id` and `page_no` to the `Page`
    model.

--- src/annotation/admin.py
+++ src/annotation/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from .models import Volume
 from .models import Page
 from .models import Annotation
 
 # Register your models here.
+admin.site.register(Volume)
 admin.site.register(Page)
 admin.site.register(Annotation)
--- src/annotation/models.py
+++ src/annotation/models.py
@@ -5,13 +5,32 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
+class Volume(models.Model):
+    """Represents a volume of the dictionary."""
+
+    id = models.AutoField(verbose_name="id", primary_key=True)
+    name = models.CharField(unique=True, null=False, max_length=128)
+
+
 class Page(models.Model):
     """Represents a pair of (page image, OCR text)."""
 
     id = models.AutoField(verbose_name="id", primary_key=True)
+    volume_id = models.ForeignKey(Volume,
+                                  on_delete=models.CASCADE,
+                                  verbose_name="volume_id")
+    page_no = models.PositiveIntegerField(verbose_name="page_no",
+                                          unique=True,
+                                          null=False)
     image_path = models.CharField(unique=True, null=False, max_length=1024)
     text = models.TextField(max_length=8192, null=False)
+
+    class Meta:
+        """Metadata of the Page model."""
+        constraints = [
+            models.UniqueConstraint(fields=["volume_id", "page_no"],
+                                    name="UX_volume_id_page_no")
+        ]
 
 
 class Annotation(models.Model):
@@ -31,7 +50,7 @@ class Annotation(models.Model):
     user_id = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
                                 verbose_name="user_id")
-    contents = models.JSONField(verbose_name="contents")
+    contents = models.TextField(verbose_name="contents", null=True)
     status = models.CharField(max_length=32,
                               choices=AnnotationStatus,
                               null=False,
