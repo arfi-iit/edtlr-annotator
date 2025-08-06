e5c20a8cf68e883b3e51928b3a34a5a7fc7085df
RePierre <petru.rebeja@gmail.com>
2025-08-06 09:00:35 +0300

    Register the route prefixing middleware in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'annotation.middleware.RoutePrefixMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
