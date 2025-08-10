dd71e01ed96f528e3f09579cb782d6520e3ba3ca
RePierre <petru.rebeja@gmail.com>
2025-08-10 10:49:33 +0300

    Added instructions to disable yapf for a file section

--- src/config/settings.py
+++ src/config/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
+# yapf: disable
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+# yapf: enable
 # Logging
 LOGGING = {
     'version': 1,
