af6ae324294a797a7481bae3bf06a4c0784689b1
RePierre <petru.rebeja@gmail.com>
2025-08-10 10:50:48 +0300

    Reverted the `STATIC_URL` to value 'static/'

--- src/config/settings.py
+++ src/config/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'annotate/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = env('STATIC_ROOT')
 
 # Default primary key field type
